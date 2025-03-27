91d97c5c3da62c58d919ca40bed7a6d66ad1211e
Blake Mosley <bmosley2@students.avc.edu>
2025-03-27 01:42:46 -0700

    added bottom bar

--- google_maps_in_flutter/lib/main.dart
+++ google_maps_in_flutter/lib/main.dart
@@ -1,36 +1,115 @@
 import 'package:flutter/material.dart';
+import 'package:stylish_bottom_bar/stylish_bottom_bar.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-void main() => runApp(const MyApp());
-
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
+void main() {
+  runApp(MyApp());
 }
 
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Stylish BottomBar Demo',
+      debugShowCheckedModeBanner: false,
+      home: BottomBarExample(),
+    );
+  }
+}
+
+class BottomBarExample extends StatefulWidget {
+  @override
+  _BottomBarExampleState createState() => _BottomBarExampleState();
+}
+
+class _BottomBarExampleState extends State<BottomBarExample> {
+  int _currentIndex = 0; // Start with Home/maps
+  final PageController _pageController = PageController(initialPage: 0);
   late GoogleMapController mapController;
 
-  final LatLng _center = const LatLng(-33.86, 151.20);
+  final LatLng _center = const LatLng(34.240547308790596, -118.52942529186363);
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
   }
 
   @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Maps Sample App'),
-          backgroundColor: Colors.green[700],
-        ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(target: _center, zoom: 11.0),
-        ),
+    return Scaffold(
+      extendBody: true,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.white,
+        child: const Icon(Icons.add, color: Colors.pink),
+        onPressed: () {
+          // this is where you add the screen to make reservations
+          // when you press the + button
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: StylishBottomBar(
+        option: AnimatedBarOptions(iconStyle: IconStyle.Default),
+        hasNotch: true,
+        fabLocation: StylishBarFabLocation.center,
+        notchStyle: NotchStyle.circle,
+        currentIndex: _currentIndex,
+        items: [
+          BottomBarItem(
+            icon: const Icon(Icons.map_outlined),
+            selectedIcon: const Icon(Icons.map),
+            title: const Text('Home'),
+            selectedColor: Colors.pink,
+            unSelectedColor: Colors.grey,
+          ),
+          BottomBarItem(
+            icon: const Icon(Icons.search_outlined),
+            selectedIcon: const Icon(Icons.search),
+            title: const Text('Search'),
+            selectedColor: Colors.pink,
+            unSelectedColor: Colors.grey,
+          ),
+          BottomBarItem(
+            icon: const Icon(Icons.book_online_outlined),
+            selectedIcon: const Icon(Icons.book_online),
+            title: const Text('Bookings'),
+            selectedColor: Colors.pink,
+            unSelectedColor: Colors.grey,
+          ),
+          BottomBarItem(
+            icon: const Icon(Icons.person_outline),
+            selectedIcon: const Icon(Icons.person),
+            title: const Text('Profile'),
+            selectedColor: Colors.pink,
+            unSelectedColor: Colors.grey,
+          ),
+        ],
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+          _pageController.jumpToPage(index);
+        },
+      ),
+      body: PageView(
+        controller: _pageController,
+        physics: const NeverScrollableScrollPhysics(),
+        children: [
+          GoogleMap(
+            onMapCreated: _onMapCreated,
+            initialCameraPosition: CameraPosition(target: _center, zoom: 16.0),
+          ),
+
+          // this is where you would add the other pages for the bottom bar
+          //right now it just makes the page say what you clicked on only the maps page works
+          const Center(child: Text('Search Page')), // Index 1
+          const Center(child: Text('Bookings Page')), // Index 2
+          const Center(child: Text('Profile Page')), // Index 3
+        ],
       ),
     );
   }
